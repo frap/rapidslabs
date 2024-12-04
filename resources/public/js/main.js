5eba8fbd7fc077fbb74dbe029ad029d8cf93e043
frap <1204361+frap@users.noreply.github.com>
2024-12-05 12:24:58 +1300

    add tailwind config

--- resources/public/js/main.js
+++ resources/public/js/main.js
@@ -1,1 +1,15 @@
 // When plain htmx isn't quite enough, you can stick some custom JS here.
+import {
+  Carousel,
+  Collapse,
+  Dropdown,
+  Ripple,
+  initTWE,
+} from "/js/tw-elements.es.min.js";
+
+// import {
+//   Ripple,
+//   initTWE,
+// } from "https://cdn.jsdelivr.net/npm/tw-elements/dist/js/tw-elements.es.min.js";
+
+initTWE({ Carousel, Collapse, Dropdown, Ripple });
--- resources/tailwind.config.js
+++ resources/tailwind.config.js
@@ -1,12 +1,15 @@
 module.exports = {
   content: [
     './src/**/*',
-    './resources/**/*',
+    // './resources/**/*',
+    "./resources/public/node_modules/tw-elements/js/**/*.js"
   ],
   theme: {
     extend: {},
   },
   plugins: [
     require('@tailwindcss/forms'),
+    require("tw-elements/plugin.cjs"),
   ],
+  darkMode: "class"
 }
